d466a53de267468563965a4f6f4445eaf4363a3f
vievieann <ifedibavivian12@gmail.com>
2023-12-04 15:23:51 -0800

    pieton

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+nomba = random.randint(-30, 30)
+if nomba > 0:
+    print(f"{nomba} is positive")
+elif number == 0:
+    print(f"{nomba} is zero")
+else:
+    print(f"{nomba} is negative")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+python programming on how to use loop, else,if et to write python codes.
